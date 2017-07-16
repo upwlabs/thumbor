98232b69cb3edf14e72a4a7df1dfe9549f6489a8
ULHI-Xin <xin@upwlabs.com>
2017-07-16 10:48:42 +0800

    basic functional blind digit wm

--- thumbor/filters/fft.py
+++ thumbor/filters/fft.py
@@ -12,6 +12,9 @@ from PIL import Image
 import numpy as np
 import scipy.fftpack as fp
 
+from PIL import ImageDraw
+from PIL import ImageFont
+
 ## Functions to go from image to frequency-image and back
 im2freq = lambda data: fp.rfft(fp.rfft(data, axis=0),
                                axis=1)
@@ -37,22 +40,49 @@ class Filter(BaseFilter):
     @filter_method()
     def fft(self):
         engine = self.context.modules.engine
-        # engine.convert_to_grayscale()
-        freq = im2freq(np.array(engine.image))
-        freq_im = arr2im(touint8(freq))
+        freq = np.fft.fft2(engine.image)
 
-        _wm = np.array(Image.open('/Users/xinzhao/Desktop/wm_laoshi.jpg'))
-        freq_wm = freq + _wm
+        # wm_im = Image.open('/Users/xinzhao/Desktop/laoshi_64.jpg')
+        # wm = Image.new('RGB', (128, 128), (0, 0, 0))
+        # wm.paste(wm_im, (0,0))
+        # wm_im = wm_im.rotate(180)
+        # wm.paste(wm_im, (64, 64, 128, 128))
+        # _wm = np.array(wm)
 
-        freq_wm_im = freq2im(freq_wm)
+        wm_im = Image.new('RGB', (128, 64), (0, 0, 0))
+        draw = ImageDraw.Draw(wm_im)
+        font = ImageFont.truetype("Hack-Regular.ttf", 32)
+        for x in (0, ):
+            for y in (0,  32, ):
+                draw.text((x, y), "CAIJI", (255, 255, 255), font=font)
+        wm = Image.new('RGB', (128, 128), (0, 0, 0))
+        wm.paste(wm_im, (0, 0))
+        wm_im = wm_im.rotate(180)
+        wm.paste(wm_im, (0, 64))
+        _wm = np.array(wm)
+
+        freq_wm = freq + _wm / _wm.max() * 255
+        # freq_wm = freq
+
+        back_from_freq = np.fft.ifft2(freq_wm)
+        back_from_freq = np.real(back_from_freq)
+
         base_size = engine.image.size
+        bz = base_size
         out = Image.new('RGB',
-                        (base_size[0] * 2, base_size[1] * 2),
+                        (bz[0] * 2, bz[1] * 2),
                         (0, 0, 0))
         out.paste(im=engine.image, box=(0, 0))
-        out.paste(im=freq_im, box=(base_size[0], 0))
-        out.paste(im=arr2im(touint8(wm_data)), box=(0, base_size[1]))
-        out.paste(im=arr2im(touint8(back_from_freq)), box=(base_size[0], base_size[1]))
+
+        back_from_freq_im = arr2im(back_from_freq.astype(int))
+        out.paste(im=back_from_freq_im, box=(bz[0], 0))
+
+        out.paste(im=arr2im(touint8(freq_wm)), box=(0, bz[1]))
+
+        freq2 = np.fft.fft2(back_from_freq_im)
+        diff = freq2 - freq
+        # out.paste(im=arr2im(diff.astype(int)), box=(bz[0], bz[1]))
+        out.paste(im=arr2im(touint8(diff)), box=(bz[0], bz[1]))
 
         engine.image = out
 
